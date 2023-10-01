300d17696a886b20914e551cea1348eab8063e48
andreistan26 <andreistan2003@gmail.com>
2023-10-01 21:49:35 +0300

    add

---
+++ nvim/lua/andrei/init.lua
@@ -0,0 +1,4 @@
+require("andrei.set")
+require("andrei.coc_config")
+require("andrei.packer")
+require("andrei.remap")
---
+++ nvim/lua/andrei/packer.lua
@@ -0,0 +1,23 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'morhetz/gruvbox'
+    use 'jremmen/vim-ripgrep'
+    use 'tpope/vim-fugitive'
+    use 'mbbill/undotree'
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use 'dhruvasagar/vim-open-url'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use ({
+        "L3MON4D3/LuaSnip",
+        tag = "v<CurrentMajor>.*", 
+        run = "make install_jsregexp"
+    })
+
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+end)
+
